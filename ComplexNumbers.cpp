492f1cc096ec29c4c425c2bd911968b61c38b70c
bbreadbread <157108707+bbreadbread@users.noreply.github.com>
2024-10-24 18:43:33 +0500

    Add files via upload

---
+++ ComplexNumbers.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+class ComplexNumbers
+{
+private:
+    double real1;
+    double imaginary1;
+
+    double real2;
+    double imaginary2;
+public:
+
+    void setNum(double real1, double imaginary1, double real2, double imaginary2) {
+
+        this->real1 = real1;
+        this->imaginary1 = imaginary1;
+
+        this->real2 = real2;
+        this->imaginary2 = imaginary2;
+    }
+    void addition() {
+
+        if (imaginary1 + imaginary2 < 0)
+            cout << real1 + real2 << imaginary1 + imaginary2 << "i";
+        else
+            cout << real1 + real2 << "+" << imaginary1 + imaginary2 << "i";
+    }
+    void subtraction() {
+
+        if (imaginary1 - imaginary2 < 0)
+            cout << real1 - real2 << imaginary1 - imaginary2 << "i";
+        else  cout << real1 - real2 << "+" << imaginary1 - imaginary2 << "i";
+    }
+    void multiplication() {
+
+        if (imaginary1 + imaginary2 < 0)
+            cout << (real1 * real2 - imaginary1 * imaginary2)
+            << (real1 * imaginary2 + imaginary1 * real2) << "i";
+        else
+            cout << (real1 * real2 - imaginary1 * imaginary2) << "+"
+            << (real1 * imaginary2 + imaginary1 * real2) << "i";
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    ComplexNumbers myComNum;
+    double a1, a2, b1, b2;
+    cout << "z1 = a1 + b1i" << endl << "z2 = a2 + b2i" << endl;
+
+    cout << endl << "a1: ";
+    cin >> a1;
+    cout << endl << "b1: ";
+    cin >> b1;
+    cout << endl << "a2: ";
+    cin >> a2;
+    cout << endl << "b2: ";
+    cin >> b2;
+    myComNum.setNum(a1, b1, a2, b2);
+
+    cout << endl << "Результат сложения: ";
+    myComNum.addition();
+
+    cout << endl << "Результат вычитания: ";
+    myComNum.subtraction();
+
+    cout << endl << "Результат умножения: ";
+    myComNum.multiplication();
+}
---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,71 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Movie {
+
+    char title[255];
+    char director[255];
+    int year;
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Movie myMovie[5];
+
+    for (int k = 0;k < 5;k++)
+    {
+        strcpy_s(myMovie[k].title, "Пусто");
+        strcpy_s(myMovie[k].director, "Пусто");
+        myMovie[k].year = 0;
+    }
+
+    int answer, i = 0, userNum;
+
+    while (true) {
+        cout << endl << "Добро пожаловать в кинотеку! Выберите действие: \n1) Добавить фильм (максимум 5) \n2) Выяснить, является ли выбранный фильм классикой. \n3) Выйти из кинотеки." << endl;
+        cin >> answer;
+        cout << endl;
+        if (answer == 1)
+        {
+            if (i < 5)
+            {
+                cout << "Название фильма:" << endl;
+                cin >> myMovie[i].title;
+
+                cout << "Гравный режиссер фильма:" << endl;
+                cin >> myMovie[i].director;
+
+                cout << "Год выхода фильма:" << endl;
+                cin >> myMovie[i].year;
+                i++;
+            }
+            else cout << "Кинотека переполнена!";
+        }
+        else if (answer == 2) 
+        {
+            cout << "Добавленные фильмы:" << endl << endl;
+
+            for (int k = 0;k < 5;k++)
+            {
+                cout << endl << "Фильм " << k + 1 << endl;
+                cout << myMovie[k].title << endl << myMovie[k].director << endl << myMovie[k].year << endl;
+            }
+
+            cout << "Введите номер фильма, который вы желаете проверить: ";
+            cin >> userNum;
+            if (myMovie[userNum - 1].year > 1910 && myMovie[userNum - 1].year < 1990)
+                cout << "Выбранный фильм является классикой." << endl << endl;
+            else  cout << "Выбранный фильм НЕ является классикой." << endl << endl;
+        }
+        else if (answer == 3) break;
+
+        else cout << "Такое действие не найдено!" << endl;
+    }
+    return 0;
+}
