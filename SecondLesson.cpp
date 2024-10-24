c7676444aa82ea2c2ae86f9266a1616566b6e2f6
bbreadbread <157108707+bbreadbread@users.noreply.github.com>
2024-10-24 14:29:24 +0500

    Add files via upload

---
+++ SecondLesson.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+void Find_Task1(int array_find[], int num_find)
+{
+    cout << "Индексы всех вхождений числа " << num_find << ": ";
+    for (int i = 0; i < 20; i++)
+    {
+        if (array_find[i] == num_find)
+        {
+            cout << i;
+            cout << " ";
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    //1
+    cout << "Задание 1" << endl;
+
+    int array[20];
+    int num;
+    for (int i = 0; i < 20; i++) {
+
+        array[i] = rand() % 10-5;
+        cout << array[i] <<" ";
+    }
+
+    cout << endl << "Введите число для поиска по массиву"<< endl;
+    cin >> num;
+    Find_Task1(array, num);
+
+    
+
+    //2
+    cout << endl << endl << "Задание 2" << endl;
+
+    int array2[3][3][3];
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum = 0;
+        cout << endl;
+        for (int j = 0; j < 3; j++) {
+            sum = 0;
+            for (int l = 0; l < 3; l++) {
+
+                array2[i][j][l] = rand() %10-5;
+                cout << "[" << i << "][" << j << "][" << l << "] =" <<
+                    array2[i][j][l] << endl;
+                sum += fabs(array2[i][j][l]);
+            }   
+            cout << "Сумма абсолютных значений глубокого массива: " << sum << endl << endl;
+        }
+    }
+
+}
---
+++ taskTwo.cpp
@@ -0,0 +1,82 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Movie {
+
+    char title[255];
+    char director[255];
+    int year;
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Movie myMovie[5];
+
+    for (int k = 0;k < 5;k++)
+    {
+        strcpy_s(myMovie[k].title, "Пусто");
+        strcpy_s(myMovie[k].director, "Пусто");
+        myMovie[k].year = 0;
+    }
+
+    int answer, i = 0, userNum;
+
+    while (true) {
+        cout << endl << "Добро пожаловать в кинотеку! Выберите действие: \n1) Добавить фильм (максимум 5) \n2) Выяснить, является ли выбранный фильм классикой. \n3) Выйти из кинотеки." << endl;
+        cin >> answer;
+        cout << endl;
+        switch (answer)
+        {
+        case 1:
+            if (i < 5)
+            {
+                cout << "Название фильма:" << endl;
+                cin >> myMovie[i].title;
+
+                cout << "Гравный режиссер фильма:" << endl;
+                cin >> myMovie[i].director;
+
+                cout << "Год выхода фильма:" << endl;
+                cin >> myMovie[i].year;
+                i++;
+            }
+            else cout << "Кинотека переполнена!";
+            break;
+
+        case 2:
+            cout << "Добавленные фильмы:" << endl << endl;
+
+            for (int k = 0;k < 5;k++)
+            {
+                cout << endl << "Фильм " << k+1 << endl;
+                cout << myMovie[k].title << endl << myMovie[k].director << endl << myMovie[k].year << endl ;
+            }
+
+            cout << "Введите номер фильма, который вы желаете проверить: ";
+            cin >> userNum;
+            if (myMovie[userNum-1].year > 1910 && myMovie[userNum-1].year < 1990)
+                cout << "Выбранный фильм является классикой." << endl << endl;
+            else  cout << "Выбранный фильм НЕ является классикой." << endl << endl;
+
+            break;
+
+        case 3:
+
+            return i;
+
+            break;
+
+        default:
+            cout << "Такое действие не найдено!" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
